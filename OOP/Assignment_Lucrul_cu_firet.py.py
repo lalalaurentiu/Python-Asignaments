f5b67d1553c7e687afd1d1c1689ecc7a00c26ed5
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2021-02-24 17:33:19 +0200

    Lucrul cu fire

---
+++ OOP/Assignment_Lucrul_cu_firet.py.py
@@ -0,0 +1,36 @@
+# Creați o clasă care se va utiliza pentru instanţierea obiectelor în formă de cub. Definiți o metodă pentru calcularea volumului unui cub și o altă metodă pentru calcularea lungimii totale a tuturor laturilor unui cub.
+# Plasați aceste metode în fire, executați mai întâi a doua metodă cu fire și, după finalizarea cu succes, executați-o pe prima. În timpul acestui proces, firul principal trebuie oprit.
+# Creați două obiecte peste care se vor apela fiecare din metodele create.
+
+Afișați rezultatele acestor metode cu comanda print.
+import threading
+
+class Cube:
+	def __init__(self, radius):
+		self.radius = radius
+		self.volume = 0
+		self.totalLeghtSides = 0
+		
+	def getVolume(self):
+		self.volume = self.radius ** 3
+		return self.volume
+		
+	def getTotalLeghtSides(self):
+		self.totalLeghtSides = 12 * self.radius
+		return self.totalLeghtSides
+		
+if __name__ == '__main__':
+	c1 = Cube(10)
+	c2 = Cube(13)
+	for cube in c1, c2:
+		t1 = threading.Thread(target=cube.getTotalLeghtSides)
+		t2 = threading.Thread(target=cube.getVolume)
+		t1.start()
+		t2.start()
+		t1.join()
+		t2.join()
+		
+		print(f'Volume is: {cube.volume}')
+		print(f'Total leght of all sides is: {cube.totalLeghtSides}')
+
+
--- OOP/Sablo_pentru_crearea_obiectelor_studenti.py
+++ OOP/Sablo_pentru_crearea_obiectelor_studenti.py
@@ -1,3 +1,14 @@
+# Este necesar să creați un șablon pentru crearea obiectelor de tip Student. Fiecare Student trebuie să aibă următoarele proprietăți: 
+# name
+# address
+# phone
+# course
+# index_number
+# Fiecare obiect care reprezintă un student trebuie să aibă următorul comportament (metodă):
+# getInfo()
+# Metoda getInfo () trebuie să returneze informații cumulative despre un student ca valoare de returnare (de exemplu, Name: John Benson, address: High Park 36, Phone: (507) 833-3567, Course: Geography, Index number: 123/007).
+# Ca să realizați șablonul pentru crearea obiectelor, utilizați o clasă.
+# În final, pe baza șablonului realizat, creați trei obiecte care să reprezinte trei studenţi cu datele dorite. Peste cele trei obiecte create, apelați metoda de imprimare a datelor - getInfo() și afișați informațiile cu comanda print.
 import datetime
 class Student:
 	__number = 1
